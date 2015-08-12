7482e2d3edb5bef13ad00b79405eee8674352f40
hajime3333out <hotta@fastbid.asia>
2015-08-13 01:07:09 +0800

    remove the call of process

--- commands/subscribe.php
+++ commands/subscribe.php
@@ -7,6 +7,13 @@ require __APP__ . "/vendor/autoload.php";
 
 use \Framework\Redis\Redis;
 use \Framework\Config;
+use Framework\Queue\Driver\RedisDriver;
+use Framework\Exception\FrameworkException;
+use Framework\Queue\QueueProcessor;
+use Framework\Input;
+use Framework\Route;
+use Framework\Queue\Driver\Driver;
+
 
 $r = Redis::getInstance();
 
@@ -21,8 +28,25 @@ if ( $process == null ) {
     try {
         $r->subscribe('cinnamon-process', function ($message, $channel) use ($r) {
             $tasks = $r->zrange('cinnamon-queue-' . $channel, 0, 10);
-            if ( count($tasks) > 0 )
-                exec(Config::get('queue.php_path') . " " . __APP__ . "/commands/receive.php > /dev/null");
+            if (count($tasks) > 0){
+
+                $rs = Config::get('driver', new RedisDriver() );
+
+                if ( !$rs instanceof Driver )
+                    throw FrameworkException::internalError('Queue Driver Not Set');
+                QueueProcessor::getInstance()->setDriver($rs)->setAsReceiver();
+
+                for ( $i = 0; $i < 10; $i++ ) {
+                    $message = $rs->receiveMessage('route');
+                    if ( ! $message ) continue;
+
+                    Input::bind($message);
+                    Route::reset();
+                    Route::setSkipMain();
+                    include __APP__ . "/route.php";
+
+                }
+            }
         });
     } catch (Exception $e ) {
         //
